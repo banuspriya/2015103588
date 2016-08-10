576ae87b05f105a8d4d22ce3a4056afff4c28e84
banuspriya <pbanu2146@gmail.com>
2016-08-10 18:21:14 +0530

    Update Date.java

--- Date.java
+++ Date.java
@@ -1,43 +1,37 @@
-package Datetime;
 public class Date {
-	
-	private int day;
-	private int year;
-	private int month;
-	
-	public Date() {
-		day=0;
-		year=0;
-		month=0;
-	}
-	
-	public int getDay() {
-		return day;
-	}
-	
-	public int getMonth() {
-		return month;
-	}
-	
-	public int getYear() {
-	 	return year;
-	}
-	
-	public void setDay(int day) {
-		this.day = day;
-	}
-	
-	public void setMonth(int month) {
-		this.month = month;
-	}
-	
-	public void setYear(int year) {
-		this.year = year;
-	}
+	int day;
+	int month;
+	int year;
 
-	public void displayDate()
-	{
-		System.out.println("The Date : "+day+"/"+month+"/"+year);
-		
+	public Date() {
+		// TODO Auto-generated constructor stub
+day=0;
+month=0;
+year=0;
 	}
+	public void setInt(int a)
+	{day=a;
+	
+	}
+	public void setMonth(int a)
+	{month=a;
+	
+	}
+	public void setYear(int a)
+	{year=a;
+	
+	}
+public int getDay()
+{return(day);
+
+}
+public int getMonth()
+{return(month);
+}
+public int getYear()
+{return(year);
+}
+public void displayDate()
+{System.out.printf("the date is %d /%d /%d ",day,month,year);
+}
 }
